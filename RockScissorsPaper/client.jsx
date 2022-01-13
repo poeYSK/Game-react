4ae22e608edbd0b7330f8c78c0ff2c288373c014
YeopMan <201802044@o.cnu.ac.kr>
2022-01-13 20:31:36 +0900

    webpack building

---
+++ RockScissorsPaper/client.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+
+import RSP from './RSPClass';
+
+const Hot = hot(RSP);
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
