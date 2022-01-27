b0e31ae0eb43c52509569522c6b29ed54c684a85
YeopMan <201802044@o.cnu.ac.kr>
2022-01-27 18:07:13 +0900

    Delete PlayingGame folder

--- PlayingGame/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-import ComprehensiveGame from './ComprehensiveGame';
-
-const Hot = hot(ComprehensiveGame);
-
-ReactDOM.render(<Hot />, document.querySelector('#root'));
